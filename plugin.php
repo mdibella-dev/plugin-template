187b86640a125014863b28869640c615bfa08ce9
mdibella-dev <mdb@marcodibella.de>
2022-08-10 18:32:19 +0200

    Add plugin main file

---
+++ plugin.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name:     <PLUGIN-TITLE>
+ * Plugin URI:      <PLUGIN-URI>
+ * Description:     <PLUGIN-DESCRIPTION>.
+ * Author:          Marco Di Bella
+ * Author URI:      https://www.marcodibella.de
+ * Version:         <PLUGIN-VERSION>
+ * Text Domain:     <PLUGIN-TEXTDOMAIN>
+ * Domain Path:     /languages
+ *
+ * @author  Marco Di Bella <mdb@marcodibella.de>
+ * @package <PLUGIN-PACKAGE>
+ */
+
+namespace PLUGIN_NAMESPACE;
+
+
+/** Prevent direct access */
+
+defined( 'ABSPATH' ) or exit;
+
+
+
+/** Variables and definitions **/
+
+$pligin_version = '1.0.0';          // eg. 1.0.0
+$plugin_path    = plugin_dir_path( __FILE__ );
+
+
+
+/** Include files */
+
+require_once( $plugin_path . 'includes/setup.php' );
