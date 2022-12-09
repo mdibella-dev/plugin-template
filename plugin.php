3022d725ec9e2bd1ef99f8e7184dff75139f720b
Marco Di Bella <mdb@marcodibella.de>
2022-12-09 11:13:42 +0100

    Change global vars to consts

--- plugin.php
+++ plugin.php
@@ -27,12 +27,12 @@ defined( 'ABSPATH' ) or exit;
 
 /** Variables and definitions **/
 
-$plugin_version = '1.0.0';          // eg. 1.0.0
-$plugin_path    = plugin_dir_path( __FILE__ );
+const PLUGIN_VERSION = '1.0.0';          // eg. 1.0.0
+const PLUGIN_DOMAIN  = '<PLUGIN-TEXTDOMAIN>';
 
 
 
 /** Include files */
 
-require_once( $plugin_path . 'includes/setup.php' );
-require_once( $plugin_path . 'includes/backend.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'includes/setup.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'includes/backend.php' );
